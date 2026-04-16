c567befed7c077254f1de9cca16eed8fbfa7dc93
qzhao19 <qi.zhao@outlook.fr>
2026-04-17 00:06:54 +0800

    rename and update

--- src/common/types/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./llm-request-config";
-export * from "./llm-model-params";
-export * from "./llm-provider";
---
+++ src/shared/types/index.ts
@@ -0,0 +1,4 @@
+export * from "./config-loader";
+export * from "./request-safety";
+export * from "./model-settings";
+export * from "./app-config";
