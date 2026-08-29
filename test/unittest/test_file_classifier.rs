3603afa473ad8a0cdf7e706f2a102bb531b06b87
qzhao19 <qi.zhao@outlook.fr>
2026-08-29 23:44:44 +0800

    update

--- test/unittest/test_file_classifier.rs
+++ test/unittest/test_file_classifier.rs
@@ -1,6 +1,6 @@
 use std::path::{Path, PathBuf};
 
-use crate::core::git::diff::classifier::FileClassifier;
+use crate::core::git::diff::FileClassifier;
 use crate::core::git::types::{
     ChangeType, ClassifiedSnapshot, FileCategory, StagedFile, StagedSnapshot,
 };
