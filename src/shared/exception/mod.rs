56931c5a5f541b6139c906642cb7c6de05bc6d79
qzhao19 <qi.zhao@outlook.fr>
2026-09-06 23:53:54 +0800

    update

--- src/shared/exception/mod.rs
+++ src/shared/exception/mod.rs
@@ -1,7 +1,9 @@
+mod app;
 mod config;
 mod git;
 mod llm;
 
+pub use app::AppError;
 pub use config::ConfigError;
-pub use git::{GitErrorCode, GitError};
+pub use git::{GitError, GitErrorCode};
 pub use llm::{LlmError, ProviderErrorType};
