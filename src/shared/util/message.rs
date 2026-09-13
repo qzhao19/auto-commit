72b00462052b5e400078295a4a72f5494e62f935
qzhao19 <qi.zhao@outlook.fr>
2026-09-13 23:43:56 +0800

    add message.rs

---
+++ src/shared/util/message.rs
@@ -0,0 +1,30 @@
+// Validate message
+
+use core::fmt;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct ValidatedCommitMessage(String);
+
+impl ValidatedCommitMessage {
+    #[inline]
+    pub fn as_str(&self) -> &str {
+        &self.0
+    }
+
+    #[inline]
+    pub fn into_string(self) -> String {
+        self.0
+    }
+}
+
+impl AsRef<str> for ValidatedCommitMessage {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+impl fmt::Display for ValidatedCommitMessage {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&self.0)
+    }
+}
